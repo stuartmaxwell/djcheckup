e4b26351725c9e3e4a5d85c44ed12ff4f126bf78
Stuart Maxwell <stuart@amanzi.nz>
2025-09-21 21:41:17 +1200

    Remove unused markdown example from output_rich.py

--- src/djcheckup/output_rich.py
+++ src/djcheckup/output_rich.py
@@ -9,15 +9,6 @@ from djcheckup.checks import CheckResult, SiteCheckResult
 
 console = Console()
 
-MARKDOWN = """
-# This is an h1
-
-Rich can do a pretty *decent* job of rendering markdown.
-
-1. This is a list item
-2. This is another list item
-"""
-
 
 def rich_output(check_results: SiteCheckResult) -> None:
     """Display results using Rich."""
